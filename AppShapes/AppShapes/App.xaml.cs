279d05d5f144b636695a4f115697bbf9affe2fa7
Elvis Geovanny Batzibal <35199976+ElvisBatzibal@users.noreply.github.com>
2020-10-24 12:10:41 -0600

    Agregar archivos de proyecto.

---
+++ AppShapes/AppShapes/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppShapes
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            Device.SetFlags(new string[] { "Shapes_Experimental"});
+            
+            MainPage = new NavigationPage ( new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
